71a9ad3ad344b60ac3684307db38f3245d9a08e1
Ho Hoang Son <23521352@gm.uit.edu.vn>
2025-10-22 12:43:27 +0700

    set up pug

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, Music App!");
+import express, { Express,Router,Request,Response } from "express";
+const app: Express = express();
+const port: number = 3000;
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
